d75572d320cc1c6b466706f4a3f2a9635dfe3b51
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2022-07-02 23:07:25 +0530

    fielas added

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,34 +1,47 @@
-import { useState } from 'react';
+import {Component} from 'react';
 import { Link } from 'react-router-dom';
 
-const LoginScreen = () => {
+class LoginScreen extends Component{
+  
+  state = {
+    email: "",
+    password: "",
+  };
 
-  const [state, setState] = useState();
-
-  return (
+  handleInputChange = (e) => {
+    this.setState({ ...this.state, [e.target.name]: e.target.value });
+    // console.log(this.state);
+  };
+  render() {
+    
+    return (
       <div className='login-screen'>
-          <h1>Enter details to login !</h1>
-          <form>
-              <div>
-                  <label>Email: </label>
-          <input type="email" name="email" onChange={(e) => {
-            console.log(e);
-            setState({ ...state, email: e.target.value });
-                  }} />
-              </div>
-              <div>
-                  <label>Password: </label>
-                  <input type="password" name="password" />
-              </div>
+        <h1>Enter details to login !</h1>
+        
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          console.log(this.state);
+        }}>
 
-              <button type='submit'>Login</button>
+          <div>
+            <label>Email: </label>
+            <input type={"email"} name={"email"} onChange={this.handleInputChange} value={this.state.email} />
+          </div>
 
-              <Link to={'/signup'}>
-                <button>Not a user? SignUp</button>
-              </Link>
-          </form>
-    </div>
-  )
+          <div>
+            <label>Password: </label>
+            <input type={"password"} name={"password"} onChange={this.handleInputChange} value={this.state.password} />
+          </div>
+
+          <button type='submit'>Login</button>
+
+          <Link to={'/signup'}>
+            <button>Not a user? SignUp</button>
+          </Link>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default LoginScreen
